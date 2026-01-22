3056916daec1e925fd8f9ed1154ec7a5570c4bf4
liuliansuchengjing <1400325886@qq.com>
2026-01-22 21:49:02 +0800

    首次提交

--- run_ner_softmax.py
+++ run_ner_softmax.py
@@ -305,20 +305,34 @@ def load_and_cache_examples(args, task, tokenizer, data_type='train'):
             examples = processor.get_dev_examples(args.data_dir)
         else:
             examples = processor.get_test_examples(args.data_dir)
-        features = convert_examples_to_features(examples=examples,
-                                                tokenizer=tokenizer,
-                                                label_list=label_list,
-                                                max_seq_length=args.train_max_seq_length if data_type=='train' \
-                                                               else args.eval_max_seq_length,
-                                                cls_token_at_end=bool(args.model_type in ["xlnet"]),
-                                                pad_on_left=bool(args.model_type in ['xlnet']),
-                                                cls_token = tokenizer.cls_token,
-                                                cls_token_segment_id=2 if args.model_type in ["xlnet"] else 0,
-                                                sep_token=tokenizer.sep_token,
-                                                # pad on the left for xlnet
-                                                pad_token=tokenizer.convert_tokens_to_ids([tokenizer.pad_token])[0],
-                                                pad_token_segment_id=4 if args.model_type in ['xlnet'] else 0,
-                                                )
+        # features = convert_examples_to_features(examples=examples,
+        #                                         tokenizer=tokenizer,
+        #                                         label_list=label_list,
+        #                                         max_seq_length=args.train_max_seq_length if data_type=='train' \
+        #                                                        else args.eval_max_seq_length,
+        #                                         cls_token_at_end=bool(args.model_type in ["xlnet"]),
+        #                                         pad_on_left=bool(args.model_type in ['xlnet']),
+        #                                         cls_token = tokenizer.cls_token,
+        #                                         cls_token_segment_id=2 if args.model_type in ["xlnet"] else 0,
+        #                                         sep_token=tokenizer.sep_token,
+        #                                         # pad on the left for xlnet
+        #                                         pad_token=tokenizer.convert_tokens_to_ids([tokenizer.pad_token])[0],
+        #                                         pad_token_segment_id=4 if args.model_type in ['xlnet'] else 0,
+        #                                         )
+        features = convert_examples_to_features(
+            examples=examples,
+            label_list=label_list,
+            max_seq_length=args.train_max_seq_length if data_type == 'train' else args.eval_max_seq_length,
+            tokenizer=tokenizer,
+            cls_token=tokenizer.cls_token,
+            sep_token=tokenizer.sep_token,
+            pad_token=tokenizer.convert_tokens_to_ids(tokenizer.pad_token),
+            pad_token_segment_id=0,
+            sequence_a_segment_id=0,
+            mask_padding_with_zero=True,
+            pad_token_label_id=-100,
+            label_all_subtokens=False,  # 你现在的实现是“只标首 subtoken”
+        )
         if args.local_rank in [-1, 0]:
             logger.info("Saving features into cached file %s", cached_features_file)
             torch.save(features, cached_features_file)
